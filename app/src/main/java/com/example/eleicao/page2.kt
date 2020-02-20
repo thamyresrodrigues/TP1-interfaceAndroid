4c18d86d19bfd05c2e5ab11584708574d7072219
thamyres.silva <thamyres.silva@live.com>
2020-02-20 10:35:42 -0300

    First commit

---
+++ app/src/main/java/com/example/eleicao/page2.kt
@@ -0,0 +1,12 @@
+package com.example.eleicao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class page2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page2)
+    }
+}
